08a516a56451d3ff2fe566fe7148d3a63392deda
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-29 23:53:23 +0300

    Task6

---
+++ Tyuiu.SabarovDA.Sprint3.Task6.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.SabarovDA.Sprint3.Task6.V6.Lib;
+namespace Tyuiu.SabarovDA.Sprint3.Task6.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int res = ds.GetSumTheDivisors(16, 24);
+            int wait = 2;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
+
---
+++ Tyuiu.SabarovDA.Sprint3.Task6.V6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.SabarovDA.Sprint3.Task6.V6.Lib;
+namespace Tyuiu.SabarovDA.Sprint3.Task6.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Сабаров Д. А. | ИИПБ-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант 6                                                               *");
+            Console.WriteLine("* Выполнил: Сабаров Дмитрий Александрович | ИИПБ-23-1                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел,                     *");
+            Console.WriteLine("* принадлежащих числовому отрезку [16, 24] количество всех делителей      *");
+            Console.WriteLine("* больше 10                                                               *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int startValue = 16;
+            int stopValue = 24;
+            Console.WriteLine(" числовой отрезок: [16,24]");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine($"Ответ:"+ ds.GetSumTheDivisors(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
