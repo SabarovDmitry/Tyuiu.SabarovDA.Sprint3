c286be26e9b7a17fcdf83c68f26a579620fffab5
Tyuiu.SabarovDA@outlook.com <Tyuiu.SabarovDA@outlook.com>
2023-10-29 23:29:42 +0300

    Task5

---
+++ Tyuiu.SabarovDA.Sprint3.Task5.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.SabarovDA.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.SabarovDA.Sprint3.Task5.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("94ce4b09-8e00-454a-9007-755c74183f1f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
